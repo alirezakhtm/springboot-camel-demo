c8631f2d368306cf9b65b474bf348f777d24ee47
Alireza Khatami Doost <alireza.khtm@gmail.com>
2019-05-26 17:42:09 +0430

    Log Camel Route added to source.

--- camel-database/src/main/java/com/khtm/test/camel/cameldatabase/ApplicationConfiguration.java
+++ camel-database/src/main/java/com/khtm/test/camel/cameldatabase/ApplicationConfiguration.java
@@ -1,8 +1,15 @@
 package com.khtm.test.camel.cameldatabase;
 
+import com.google.gson.GsonBuilder;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -17,10 +24,22 @@ public class ApplicationConfiguration {
             @Override
             public void configure() throws Exception {
                 from("sql:select id from testdb.tbl_user where status='" + OrderStatus.PENDING + "'" +
-                        "?consumer.onConsume=update testdb.tbl_user set status='" + OrderStatus.CAMCELED + "' where id=:#id")
+                        "?consumer.onConsume=update testdb.tbl_user set status='" + OrderStatus.CANCELED + "' where id=:#id")
                         .id("Camel-Database-Bean")
                         .beanRef("orderItemMessageTranslator", "transformToOrderItemMessage")
                         .to("log:com.khtm.test.camel.cameldatabase.User?level=INFO");
+
+                from("sql:select * from testdb.tbl_user where status='" + OrderStatus.NEW + "'")
+                        .id("Camel-Database-Logger")
+                        .process(new Processor() {
+                            @Override
+                            public void process(Exchange exchange) throws Exception {
+                                LoggerFactory.getLogger(">>>>>> TEST - BODY >>>>>>>>").info(
+                                        exchange.getIn().getBody(String.class)
+                                );
+                            }
+                        })
+                        .log(">>>> Camel Direct");
             }
         };
     }
